88ec0b9372e695085cdcd2f400b6ffcb37319ec7
J-Kummer <justine.kummer@snhu.edu.com>
2026-07-07 06:59:45 -0500

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('app_server/routes/index');
-var usersRouter = require('app_server/routes/users');
-var travelRouter = require('app_server/routes/travel');
-const { handlebars } = require('hbs');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+const handlebars = require('hbs');
 
 var app = express();
 
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
-//register handlebars
+//register handlebar
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
