a528c06ff105a44d80f732198b0ac01c0b167e88
J-Kummer <justine.kummer@snhu.edu.com>
2026-08-08 16:20:31 -0500

    Module 6 completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -38,7 +38,74 @@ if(!q)
 }
 };
 
+
+
+const tripsAddTrip = async (req, res) => {
+    const mongoData = {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description:req.body.description
+    };
+
+    const q = await Model.create(mongoData);
+
+    if (!q) {
+        return res.status(400).json({message: 'DB error'});
+    } else {
+        return res.status(201).json(q)
+    }
+
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip 
+// Regardless of outcome, response must include HTML status code 
+// and JSON message to the requesting client 
+const tripsUpdateTrip = async(req, res) => { 
+ 
+    // Uncomment for debugging 
+    console.log(req.params); 
+    console.log(req.body); 
+ 
+    const q = await Model 
+        .findOneAndUpdate( 
+            { 'code' : req.params.tripCode }, 
+            { 
+                code: req.body.code, 
+                name: req.body.name, 
+                length: req.body.length, 
+                start: req.body.start, 
+                resort: req.body.resort, 
+                perPerson: req.body.perPerson, 
+                image: req.body.image, 
+                description: req.body.description 
+            }  
+        ) 
+        .exec(); 
+         
+        if(!q) 
+        { // Database returned no data 
+            return res 
+                .status(400) 
+                .json({message: 'trip not found' });
+                } else { // Return resulting updated trip 
+            return res 
+                .status(201) 
+                .json(q); 
+        }     
+                
+        // Uncomment the following line to show results of operation 
+        // on the console 
+        // console.log(q); 
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
